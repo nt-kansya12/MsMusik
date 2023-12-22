d61c786bdc6f9ce3c96eec8b4b4d23d6856a2fa1
nt-kansya12 <153280158+nt-kansya12@users.noreply.github.com>
2023-12-22 17:41:14 +0700

    T config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 # JANGAN HAPUS YA KONTOL
-OWNER_ID.append(5573141376)
+OWNER_ID.append(1499130346)
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
@@ -59,7 +59,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DjeFaris/RitoMusik",
+    "https://github.com/nt-kansya12/RitoMusik",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
